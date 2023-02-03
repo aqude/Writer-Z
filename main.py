31abfe75b2a77557f633f57262936532ab907666
Sergey Tovpeko <aqude@localhost.localdomain>
2023-02-03 22:14:18 +0300

    final push

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ import os.path
 API_TOKEN = '5797067852:AAH64nToAYrdRYMFznVFiLUdcPYRwLq5BRM'
 
 bot = telebot.TeleBot(API_TOKEN)
+
+# файл для чтения
 file_path_for_edit = 'test.txt'
 checkFile = os.path.exists(file_path_for_edit)
+
+# файл для записи и удаления
+fileCreate = 'createfile.txt'
 
 
 @bot.message_handler(commands=['help', 'start'])
@@ -20,11 +25,12 @@ def send_welcome(message):
 
 @bot.message_handler(content_types=['text'])
 def send_welcome(message):
-    if message.text == '/reg':
-        bot.send_message(message.chat.id, "Пришлите текст")
-        bot.register_next_step_handler(message, text_filter)
+    if message.text == '/z':
+        msg_user = bot.send_message(message.user.id, "Пришлите текст")
+        bot.register_next_step_handler(msg_user, text_filter)
     else:
-        bot.send_message(message.chat.id, 'Напиши /reg');
+        bot.send_message(message.user.id, 'Напиши /z')
+
 
 # def edit_word(message):
 #
@@ -33,31 +39,35 @@ def send_welcome(message):
 
 def text_filter(message):
     if checkFile:
-        with open(file_path_for_edit, 'w') as f:
-            f.write(message)
+        with open(file_path_for_edit, 'w+') as f:
+            f.write(message.text)
+    else:
+        print("not found file")
 
     # else:
     #     with open(file_path_for_edit, 'w') as f:
     #         f.write(message)
-    with open('test.txt', 'r') as f:
+    with open(file_path_for_edit, 'r') as f:
         context = f.read()
         filter_ = context.replace('з', 'Z')
         # my_file = open(file_path_for_edit, "w+")
 
-    with open('createfile.txt', 'w') as f:
+    with open(fileCreate, 'w') as f:
         f.write(filter_)
 
-        with open('createfile.txt', 'r') as file:
-            print(file.read())
-
-        fileCreate = 'createfile.txt'
+    with open(fileCreate, 'r') as file:
+        data = file.read().replace('\n', '')
 
     with open('createfile.txt', 'r') as f:
-        w = f
+        w = f.read()
 
-    bot.send_message(message.chat.id, w)
+    bot.send_message(message.user.id, w)
 
+    # удаляем файл
     os.remove(fileCreate)
+    # отчищаем файл
+    with open(file_path_for_edit, 'w') as f:
+        f.write('')
 
 
 bot.infinity_polling()
